dd4858960c57c909b75c85aac7af5d8fc1d00012
Sergey Azarkevich <azarkevich@gmail.com>
2013-10-16 11:58:40 +0300

    add partial support of araxis merge

--- VSS3WayMerge/MergerToolDefinition.cs
+++ VSS3WayMerge/MergerToolDefinition.cs
@@ -5,7 +5,6 @@
 		public string Name;
 		public string Exe;
 		public string Diff;
-		public string Diff3;
 		public string Merge;
 
 		public override string ToString()
--- VSS3WayMerge/MergerToolsManager.cs
+++ VSS3WayMerge/MergerToolsManager.cs
@@ -8,7 +8,6 @@
 				Name = "TortoiseMerge",
 				Exe = @"C:\Program Files\TortoiseSVN\bin\TortoiseMerge.exe",
 				Diff = "/base:$LEFT$ /mine:$RIGHT$",
-				Diff3 = "/base:$BASE$ /theirs:$LEFT$ /mine:$RIGHT$",
 				Merge = "/base:$BASE$ /theirs:$LEFT$ /mine:$RIGHT$ /merged:$MERGED$",
 			};
 
@@ -16,7 +15,6 @@
 				Name = "Perforce P4Merge",
 				Exe = @"C:\Program Files\Perforce\p4merge.exe",
 				Diff = "$LEFT$ $RIGHT$",
-				Diff3 = "$BASE$ $LEFT$ $RIGHT$",
 				Merge = "$BASE$ $LEFT$ $RIGHT$ $MERGED$",
 			};
 
@@ -24,7 +22,6 @@
 				Name = "Beyond Compare 3",
 				Exe = @"C:\Program Files (x86)\Beyond Compare 3\BCompare.exe",
 				Diff = "$LEFT$ $RIGHT$ \"/lefttitle=$LTITLE$\" \"/righttitle=$RTITLE$\"",
-				Diff3 = "$LEFT$ $RIGHT$ $BASE$ \"/lefttitle=$LTITLE$\" \"/righttitle=$RTITLE$\" \"/centertitle=$BTITLE$\"",
 				Merge = "$LEFT$ $RIGHT$ $BASE$ $MERGED$ \"/lefttitle=$LTITLE$\" \"/righttitle=$RTITLE$\" \"/centertitle=$BTITLE$\" \"/outputtitle=$MTITLE$\"",
 			};
 
@@ -32,11 +29,17 @@
 				Name = "TortoiseGit",
 				Exe = @"C:\Program Files\TortoiseGit\bin\TortoiseGitMerge.exe",
 				Diff = "/base:$LEFT$ /mine:$RIGHT$",
-				Diff3 = "/base:$BASE$ /theirs:$LEFT$ /mine:$RIGHT$",
 				Merge = "/base:$BASE$ /theirs:$LEFT$ /mine:$RIGHT$ /merged:$MERGED$",
 			};
 
-			return new[] { bcmp, tsvn, p4merge, tgit };
+			var araxis = new MergerToolDefinition {
+				Name = "Araxis Merge (look like 3-way merge not ready)",
+				Exe = @"C:\Program Files\Araxis\Araxis Merge\compare.exe",
+				Diff = "/max /wait $LEFT$ $RIGHT$ /title1:\"$LTITLE$\" /title2:\"$RTITLE$\"",
+				Merge = "/a2 /3 /max /wait $LEFT$ $BASE$ $RIGHT$ $MERGED$ /title1:\"$LTITLE$\" /title2:\"$BTITLE$\" /title3:\"$RTITLE$\""
+			};
+
+			return new[] { bcmp, araxis, p4merge, tsvn, tgit };
 		}
 	}
 }
