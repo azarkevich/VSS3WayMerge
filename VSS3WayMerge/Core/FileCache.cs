37c2057df26b3d5231f36d9ff497abfcd3d472a4
Sergey Azarkevich <azarkevich@gmail.com>
2014-09-09 23:40:55 +0300

    simplify file cache to single data file.

--- VSS3WayMerge/Core/FileCache.cs
+++ VSS3WayMerge/Core/FileCache.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Globalization;
 using System.IO;
-using System.Linq;
 using System.Text;
 
 namespace vcslib
@@ -16,19 +14,15 @@ namespace vcslib
 			// Some notes
 			public readonly string Notes;
 
-			// path to content
-			public readonly string PackPath;
-
 			// offset to content
 			public readonly long Offset;
 
 			// content size
 			public readonly int Size;
 
-			public CacheEntry(string key, string notes, string packPath, long offset, int size)
+			public CacheEntry(string key, string notes, long offset, int size)
 			{
 				Key = key;
-				PackPath = packPath;
 				Notes = notes;
 				Offset = offset;
 				Size = size;
@@ -59,38 +53,35 @@ namespace vcslib
 			while ((line = reader.ReadLine()) != null)
 			{
 				var arr = line.Split('\t');
-				if (arr.Length != 5)
+				if (arr.Length != 4)
 				{
 					Console.Error.WriteLine("Bad cache line: " + line);
 					Environment.Exit(-1);
 				}
 
-				for (var i = 0; i < 3; i++)
+				for (var i = 0; i < 2; i++)
 				{
 					if(arr[i] == "")
 						arr[i] = null;
 				}
 
-				var ce = new CacheEntry(arr[0], arr[1], arr[2], long.Parse(arr[3]), int.Parse(arr[4]));
+				var ce = new CacheEntry(arr[0], arr[1], long.Parse(arr[2]), int.Parse(arr[3]));
 
 				_cacheIndex[ce.Key] = ce;
 			}
 
-			var fname = DateTimeOffset.Now.Ticks.ToString(CultureInfo.InvariantCulture);
-
-			_packPath = Path.Combine(baseDir, "" + fname.Last(), fname);
-			Directory.CreateDirectory(Path.GetDirectoryName(_packPath));
-
-			_packWrite = new FileStream(_packPath, FileMode.OpenOrCreate, FileAccess.Write, FileShare.ReadWrite);
+			_packWrite = new FileStream(Path.Combine(baseDir, "data"), FileMode.OpenOrCreate, FileAccess.Write, FileShare.ReadWrite);
 			_packWrite.Seek(0, SeekOrigin.End);
+
+			_packRead = new FileStream(Path.Combine(baseDir, "data"), FileMode.Open, FileAccess.Read, FileShare.ReadWrite);
 		}
 
-		string _packPath;
 		Stream _packWrite;
+		Stream _packRead;
 
 		void WriteIndexEntry(CacheEntry ce)
 		{
-			_indexWriter.WriteLine("{0}	{1}	{2}	{3}	{4}", ce.Key, ce.Notes, ce.PackPath, ce.Offset, ce.Size);
+			_indexWriter.WriteLine("{0}	{1}	{2}	{3}", ce.Key, ce.Notes, ce.Offset, ce.Size);
 			_indexWriter.Flush();
 		}
 
@@ -98,7 +89,7 @@ namespace vcslib
 		{
 			lock (this)
 			{
-				var ce = new CacheEntry(key, null, _packPath, _packWrite.Position, data.Length);
+				var ce = new CacheEntry(key, null, _packWrite.Position, data.Length);
 				
 				_packWrite.Write(data, 0, data.Length);
 				_packWrite.Flush();
@@ -111,18 +102,15 @@ namespace vcslib
 
 		public byte[] GetChunk(string key)
 		{
-			CacheEntry ce;
 			lock (this)
 			{
+				CacheEntry ce;
 				if (!_cacheIndex.TryGetValue(key, out ce))
 					return null;
-			}
 
-			using (var fs = new FileStream(ce.PackPath, FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
-			{
 				var buff = new byte[ce.Size];
-				fs.Seek(ce.Offset, SeekOrigin.Begin);
-				fs.Read(buff, 0, ce.Size);
+				_packRead.Seek(ce.Offset, SeekOrigin.Begin);
+				_packRead.Read(buff, 0, ce.Size);
 
 				return buff;
 			}
